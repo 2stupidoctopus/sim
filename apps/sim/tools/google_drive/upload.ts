12fffe9ff3ac00d1f8313bb3bb74a4dee7f34e75
Adam Gough <adamgough@Mac-530.lan>
2025-10-25 00:47:45 -0700

    added back file scope

--- apps/sim/tools/google_drive/upload.ts
+++ apps/sim/tools/google_drive/upload.ts
@@ -14,7 +14,10 @@ export const uploadTool: ToolConfig<GoogleDriveToolParams, GoogleDriveUploadResp
   oauth: {
     required: true,
     provider: 'google-drive',
-    additionalScopes: ['https://www.googleapis.com/auth/drive.readonly'],
+    additionalScopes: [
+      'https://www.googleapis.com/auth/drive.readonly',
+      'https://www.googleapis.com/auth/drive.file',
+    ],
   },
 
   params: {
